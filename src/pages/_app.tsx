37fc3adcaffd68fa716ae3cb01f8ede81516454d
RakibAhmed01 <rakibahmed34360@gmail.com>
2023-07-28 14:56:29 +0600

    chore: initial setup

---
+++ src/pages/_app.tsx
@@ -0,0 +1,47 @@
+import {
+  ColorScheme,
+  ColorSchemeProvider,
+  MantineProvider,
+} from '@mantine/core';
+import { AppProps } from 'next/app';
+import { Karla } from 'next/font/google';
+import Head from 'next/head';
+import { useState } from 'react';
+
+const karla = Karla({ subsets: ['latin'] });
+
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props;
+
+  const [colorScheme, setColorScheme] = useState<ColorScheme>('light');
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+
+  return (
+    <>
+      <Head>
+        <title>PC Builder</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+
+      <ColorSchemeProvider
+        colorScheme={colorScheme}
+        toggleColorScheme={toggleColorScheme}
+      >
+        <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+          theme={{
+            colorScheme: 'dark',
+            fontFamily: karla.style.fontFamily,
+          }}
+        >
+          <Component {...pageProps} />
+        </MantineProvider>
+      </ColorSchemeProvider>
+    </>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@mantine/core';
+
+export default function Home() {
+  return (
+    <div>
+      <p className="text-red-600">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis nulla
+        amet mollitia totam temporibus, dolorem, natus, iste necessitatibus
+        similique assumenda perferendis dicta consequuntur officiis voluptatum!
+      </p>
+      <Button variant="light">Click me</Button>
+    </div>
+  );
+}
